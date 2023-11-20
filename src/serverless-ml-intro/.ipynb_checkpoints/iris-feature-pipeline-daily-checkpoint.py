a2280b0699e2c22d30cb27597400406e1d4a3ea0
dengty1998 <dengtianyu98@gmail.com>
2023-11-21 00:12:18 +0200

    feat, fix and run all pipelines

--- src/serverless-ml-intro/.ipynb_checkpoints/iris-feature-pipeline-daily-checkpoint.py
+++ src/serverless-ml-intro/.ipynb_checkpoints/iris-feature-pipeline-daily-checkpoint.py
@@ -1,18 +1,18 @@
 import os
 import modal
 
-LOCAL = False
+LOCAL=False
 
 if LOCAL == False:
-    stub = modal.Stub("iris_daily")
-    image = modal.Image.debian_slim().pip_install(["hopsworks"])
+   stub = modal.Stub("iris_daily")
+   image = modal.Image.debian_slim().pip_install(["hopsworks"]) 
 
-    @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("HOPSWORKS_API_KEY"))
-    def f():
-        g()
+   @stub.function(image=image, schedule=modal.Period(days=1), secret=modal.Secret.from_name("id2223"))
+   def f():
+       g()
 
 
-def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_width_min,
+def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_width_min, 
                     petal_len_max, petal_len_min, petal_width_max, petal_width_min):
     """
     Returns a single iris flower as a single row in a DataFrame
@@ -20,11 +20,11 @@ def generate_flower(name, sepal_len_max, sepal_len_min, sepal_width_max, sepal_w
     import pandas as pd
     import random
 
-    df = pd.DataFrame({"sepal_length": [random.uniform(sepal_len_max, sepal_len_min)],
+    df = pd.DataFrame({ "sepal_length": [random.uniform(sepal_len_max, sepal_len_min)],
                        "sepal_width": [random.uniform(sepal_width_max, sepal_width_min)],
                        "petal_length": [random.uniform(petal_len_max, petal_len_min)],
                        "petal_width": [random.uniform(petal_width_max, petal_width_min)]
-                       })
+                      })
     df['variety'] = name
     return df
 
@@ -36,14 +36,12 @@ def get_random_iris_flower():
     import pandas as pd
     import random
 
-    virginica_df = generate_flower(
-        "Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
-    versicolor_df = generate_flower(
-        "Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
-    setosa_df = generate_flower("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
+    virginica_df = generate_flower("Virginica", 8, 5.5, 3.8, 2.2, 7, 4.5, 2.5, 1.4)
+    versicolor_df = generate_flower("Versicolor", 7.5, 4.5, 3.5, 2.1, 3.1, 5.5, 1.8, 1.0)
+    setosa_df =  generate_flower("Setosa", 6, 4.5, 4.5, 2.3, 1.2, 2, 0.7, 0.3)
 
     # randomly pick one of these 3 and write it to the featurestore
-    pick_random = random.uniform(0, 3)
+    pick_random = random.uniform(0,3)
     if pick_random >= 2:
         iris_df = virginica_df
         print("Virginica added")
@@ -66,13 +64,13 @@ def g():
 
     iris_df = get_random_iris_flower()
 
-    iris_fg = fs.get_feature_group(name="iris", version=1)
+    iris_fg = fs.get_feature_group(name="iris",version=1)
     iris_fg.insert(iris_df)
 
-
 if __name__ == "__main__":
-    if LOCAL == True:
+    if LOCAL == True :
         g()
     else:
-        modal.runner.deploy_stub(stub)
-        f.remote()
+        stub.deploy("iris_daily")
+        with stub.run():
+            f()
